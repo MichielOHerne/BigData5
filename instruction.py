99dd556dc0c63ab1cee1adf67570bbeede99e4e9
Tim <19531735+TimAl@users.noreply.github.com>
2017-12-07 20:33:44 +0100

    Create txt-files with messages per fence
    - Created a extended Merge function (Merge_ext), which also stores all the data when merging, slower than the normal Merge, but useful for further computations.
    - In the instruction-file, a file will be generated per hashtag containing every tweet with that specific hashtag (as Casper wishes).
    - User has to confirm that a number of text-files will be generated.
    - All generated files are stored under "E:\dump".
    - The dump-folder is wiped COMPLETELY every time new data is stored (clear_dump).
    - A clean_str function is made, which removes characters that are causing errors when writing to a file otherwise.

--- instruction.py
+++ instruction.py
@@ -2,20 +2,32 @@ import filters as f
 import operations as operation
 import create_selection as cs
 
+dumpfolder = "E:/dump/"
 received = [cs.Select_data([], info = True)]
-received = received + cs.M_select_data("E:/20111231_20/", 0, 10,                    \
+received = received + cs.M_select_data("E:/20111231_20/", 0, 60,                    \
                         f_htg=(lambda x: f.all_without_none(x)),                    \
-                        f_rtc=(lambda x: f.larger_than(x, 0)),                      \
-                        f_txt=(lambda x: f.consists_of_and(x,["year", "new"])),     \
-                        f_cre=(lambda x: f.consists_of(x,"sun")))
+                        f_txt=(lambda x: f.consists_of_and(x,["year", "new"])))
 #received = received + cs.M_select_data("E:/20111231_21/", 0, 60)
 #received = received + cs.M_select_data("E:/20111231_22/", 0, 60)
 #received = received + cs.M_select_data("E:/20111231_23/", 0, 60)
 
-filtered = operation.Merge(received, "Hashtag")
-filtered = operation.RemoveUnimportant(filtered, 2, 1)
+filtered = operation.Merge_ext(received, "Hashtag")
+filtered = operation.RemoveUnimportant(filtered, 4, 1)
 filtered = operation.SortDec(filtered, 1)
 
-for item in received:
-    print(item)
+input("\tThe program will create " + str(len(filtered) - 1) + " files. \n\tAll files from the folder " + str(dumpfolder) + " will be DELETED.\n\tPress 'Enter' to continue... ")  # Ask user
 
+operation.clear_dump(dumpfolder)  # Delete all the contents from the dump-folder
+print("Old dump data deleted")
+
+#print(filtered[0])
+for i in range(1, len(filtered)):
+    #print(filtered[i][0], filtered[i][1])
+    output = open(dumpfolder+filtered[i][0]+".txt", "a")
+    for j in range(0, len(filtered[i][2])):
+        #print("\t", filtered[i][2][j][1])
+        output.write(operation.clean_str(filtered[i][2][j][1], True))
+        output.write("\n")
+    output.close()
+    print("File " + filtered[i][0] + ".txt created")
+print("Finished")
--- operations.py
+++ operations.py
@@ -1,3 +1,5 @@
+import os
+
 def Merge(all_data, by):
     column = None
     unique_items = list()
@@ -8,9 +10,7 @@ def Merge(all_data, by):
             break
     else:
         print("No column named " + by + " in the header of the data")
-
     unique_items.append([by, "Occurance"])
-
     for i in range(1, len(all_data)):
         found_item = False
         for j in range(1, len(unique_items)):
@@ -19,7 +19,29 @@ def Merge(all_data, by):
                 found_item = True
         if not found_item:
             unique_items.append([all_data[i][column], 1])
+    return unique_items
 
+
+def Merge_ext(all_data, by):
+    column = None
+    unique_items = list()
+    for i in range(len(all_data[0])):
+        if all_data[0][i] == by:
+            column = i
+            print("Merge by ", by, " in column ", str(column))
+            break
+    else:
+        print("No column named " + by + " in the header of the data")
+    unique_items.append([by, "Occurance", "All Tweet-data in a list"])
+    for i in range(1, len(all_data)):
+        found_item = False
+        for j in range(1, len(unique_items)):
+            if (unique_items[j][0] == all_data[i][column]):
+                unique_items[j][1] = unique_items[j][1] + 1
+                unique_items[j][2].append(all_data[i])
+                found_item = True
+        if not found_item:
+            unique_items.append([all_data[i][column], 1, [all_data[i]]])
     return unique_items
 
 
@@ -42,3 +64,26 @@ def SortDec(data, col):
     new_data.append(data[0])
     new_data.extend(sorted(data[1:], key=lambda data: -data[col]))
     return new_data
+
+
+
+def clean_str(s, show_ignored_characters):
+    new_s = str()
+    for char in s:
+        if (ord(char) > 31 and ord(char) < 256):
+            new_s = new_s + char
+        else:
+            if show_ignored_characters:
+                print("Invalid character '" + char + "' ignored")
+    return new_s
+
+
+
+def clear_dump(location):
+    for the_file in os.listdir(location):
+        file_path = os.path.join(location, the_file)
+        try:
+            if os.path.isfile(file_path):
+                os.unlink(file_path)
+        except Exception as e:
+            print(e)

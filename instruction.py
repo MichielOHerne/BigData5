06b60261d4e73df4097433a9af04764d9b926f8b
Tim <19531735+TimAl@users.noreply.github.com>
2017-12-07 15:12:39 +0100

    Operations are working
    With modified instruction-script to test the result

--- instruction.py
+++ instruction.py
@@ -1,8 +1,9 @@
 import filters as f
+import operations as operation
 import create_selection as cs
 
 received = [cs.Select_data([], info = True)]
-received = received + cs.M_select_data("E:/20111231_20/", 0, 16,        \
+received = received + cs.M_select_data("E:/20111231_20/", 0, 60,        \
                         f_htg=(lambda x: f.all_without_none(x)),        \
                         f_rtc=(lambda x: f.larger_than(x, 0)),          \
                         f_txt=(lambda x: f.consists_of(x,"year")),      \
@@ -11,6 +12,10 @@ received = received + cs.M_select_data("E:/20111231_20/", 0, 16,        \
 #received = received + cs.M_select_data("E:/20111231_22/", 0, 60)
 #received = received + cs.M_select_data("E:/20111231_23/", 0, 60)
 
-for item in received:
+filtered = operation.Merge(received, "Hashtag")
+filtered = operation.RemoveUnimportant(filtered, 2, 1)
+filtered = operation.SortDec(filtered, 1)
+
+for item in filtered:
     print(item)
 
--- operations.py
+++ operations.py
@@ -7,11 +7,13 @@ def Merge(all_data, by):
             print("Merge by ", by, " in column ", str(column))
             break
     else:
-        print("No " + by)
+        print("No column named " + by + " in the header of the data")
+
+    unique_items.append([by, "Occurance"])
 
     for i in range(1, len(all_data)):
         found_item = False
-        for j in range(len(unique_items)):
+        for j in range(1, len(unique_items)):
             if (unique_items[j][0] == all_data[i][column]):
                 unique_items[j][1] = unique_items[j][1] + 1
                 found_item = True
@@ -24,8 +26,9 @@ def Merge(all_data, by):
 
 def RemoveUnimportant(all_data, threshold, column):
     new_data = list()
+    new_data.append(all_data[0])
     numdel = 0
-    for i in range(len(all_data)):
+    for i in range(1, len(all_data)):
         if all_data[i][column] > threshold - 1:
             new_data.append(all_data[i])
         else: numdel = numdel + 1
@@ -35,4 +38,7 @@ def RemoveUnimportant(all_data, threshold, column):
 
 
 def SortDec(data, col):
-    return sorted(data, key=lambda data: -data[col])
+    new_data = []
+    new_data.append(data[0])
+    new_data.extend(sorted(data[1:], key=lambda data: -data[col]))
+    return new_data

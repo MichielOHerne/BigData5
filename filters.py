a9e242749eefe774672bfbf31101deccd918ccaf
Tim <19531735+TimAl@users.noreply.github.com>
2017-12-07 15:36:06 +0100

    Extended filters
    Added two filters, witch are a extension of the "consist_of". 
    consists_of  only selects messages which contains a specific word
    consists_of_or  only selects messages which contains one of the words in an array of specified words [new]
    consists_of_and  only selects messages which contains all of the words in an array of specified words [new]
    
    Also a new version on the instruction file with an example how to use/ to test it

--- filters.py
+++ filters.py
@@ -1,111 +1,68 @@
 # Om te gebruiken:
 # import filters as filter
 
-def country(all_data, country_code):
-    column = None
-    new_data = []
-    for i in range(len(all_data[0])):
-        if all_data[0][i] == "Place":
-            column = i
-            break
-    else:
-        print("No \"Place\"")
-    new_data.append(all_data[0])
-    if column != None:
-        for i in range(1, len(all_data)):
-            if all_data[i][column]['country_code'] == country_code:
-                new_data.append(all_data[i])
-    return new_data
-
-def followers(all_data, min_nof = 0, max_nof = 9999):
-    column = None
-    new_data = []
-    for i in range(len(all_data[0])):
-        if all_data[0][i] == "U: Followers count":
-            column = i
-            break
-    else:
-        print("No \"U: Followers count\"")
-    new_data.append(all_data[0])
-    if column != None:
-        for i in range(1, len(all_data)):
-            if (all_data[i][column] > min_nof - 1) and (all_data[i][column] < max_nof + 1):
-                new_data.append(all_data[i])
-    return new_data
-
-def friends(all_data, min_nof = 0, max_nof = 9999):
-    column = None
-    new_data = []
-    for i in range(len(all_data[0])):
-        if all_data[0][i] == "U: Friends count":
-            column = i
-            break
-    else:
-        print("No \"U: Friends count\"")
-    new_data.append(all_data[0])
-    if column != None:
-        for i in range(1, len(all_data)):
-            if (all_data[i][column] > min_nof - 1) and (all_data[i][column] < max_nof + 1):
-                new_data.append(all_data[i])
-    return new_data
-
-def contain_word(all_data, word):
-    column = None
-    new_data = []
-    for i in range(len(all_data[0])):
-        if all_data[0][i] == "Text":
-            column = i
-            break
-    else:
-        print("No \"Text\"")
-    new_data.append(all_data[0])
-    if column != None:
-        for i in range(1, len(all_data)):
-            if all_data[i][column] != None:
-                words = all_data[i][column].lower().split()
-                if word in words:
-                    new_data.append(all_data[i])
-    return new_data
-
-
-
-
-
-# Nieuw:
 def all(input):
+    # Everything is excepted
     return True
 
 def all_without_none(input):
-    if input == None:
+    # Every item is excepted, as long as it is something
+    if input is None:
         return False
     return True
 
 def equal_to(input, sameas):
+    # Only accept items that are the same as
     if input == sameas:
         return True
     return False
 
 def larger_than(input, threshold):
+    # Only accept integers greater than the threshold
     if isinstance(input, str):
         return True
-    elif input == None:
+    elif input is None:
         return False
     if input > threshold:
         return True
     return False
 
 def in_between(input, min, max):
+    # Only accept integers within the given limits
     if isinstance(input, str):
         return True
-    elif input == None:
+    elif input is None:
         return False
     if input > min and input < max:
         return True
     return False
 
 def consists_of(input, included):
+    # Only accept if the specified word is mentioned
     if isinstance(input, str):
         words =input.lower().split()
         if included.lower() in words:
             return True
     return False
+
+def consists_of_or(input, included_list):
+    # Same as consist_of, but now for a array of specified words, one of them should match
+    for word in included_list:
+        if isinstance(input, str):
+            words =input.lower().split()
+            if word.lower() in words:
+                return True
+    return False
+
+def consists_of_and(input, included_list):
+    # Same as consist_of, but now for a array of specified words, all of them must match
+    counter = 0
+    for word in included_list:
+        if isinstance(input, str):
+            words =input.lower().split()
+            if word.lower() in words:
+                counter = counter + 1
+    if counter == len(included_list):
+        return True
+    else:
+        return False
--- instruction.py
+++ instruction.py
@@ -3,10 +3,10 @@ import operations as operation
 import create_selection as cs
 
 received = [cs.Select_data([], info = True)]
-received = received + cs.M_select_data("E:/20111231_20/", 0, 60,        \
-                        f_htg=(lambda x: f.all_without_none(x)),        \
-                        f_rtc=(lambda x: f.larger_than(x, 0)),          \
-                        f_txt=(lambda x: f.consists_of(x,"year")),      \
+received = received + cs.M_select_data("E:/20111231_20/", 0, 10,                    \
+                        f_htg=(lambda x: f.all_without_none(x)),                    \
+                        f_rtc=(lambda x: f.larger_than(x, 0)),                      \
+                        f_txt=(lambda x: f.consists_of_and(x,["year", "new"])),     \
                         f_cre=(lambda x: f.consists_of(x,"sun")))
 #received = received + cs.M_select_data("E:/20111231_21/", 0, 60)
 #received = received + cs.M_select_data("E:/20111231_22/", 0, 60)
@@ -16,6 +16,6 @@ filtered = operation.Merge(received, "Hashtag")
 filtered = operation.RemoveUnimportant(filtered, 2, 1)
 filtered = operation.SortDec(filtered, 1)
 
-for item in filtered:
+for item in received:
     print(item)
 

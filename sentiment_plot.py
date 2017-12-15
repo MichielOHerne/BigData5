d2a518261a18e149a6c6fd41f234be07cddc1970
Tim <19531735+TimAl@users.noreply.github.com>
2017-12-15 13:03:57 +0100

    Added bar-graph
    Super-fancy.

--- sentiment_plot.py
+++ sentiment_plot.py
@@ -13,13 +13,35 @@ def plot_line(data_list):
         x_axis.append(datetime.datetime.fromtimestamp(data_list[i][2]).strftime('%Y-%m-%d %H:%M:%S'))  # Timestamp
         y_axis.append(data_list[i][1][0])  # Compound
         messages.append(data_list[i][0])   # Message
-        total = total + data_list[i][1][0]
+        total = total + data_list[i][1][0] # Total compound
     line = Scatter(x=x_axis, y=y_axis, text=messages, line=dict(shape='linear'), name='Positive/ Negative compound')
     avg = total/(len(data_list)-1)
-    avg_ref = Scatter(x=[x_axis[0], x_axis[len(x_axis)-1]], y=[avg]*2, text=['Average']*2, line=dict(shape='spline'), name='Average')
+    avg_ref = Scatter(x=[x_axis[0], x_axis[len(x_axis)-1]], y=[avg]*2, text=['Average']*2, line=dict(shape='spline', dash = 'dash'), name='Average')
     layout = dict(title = 'Data for <b>' + name +'</b>', xaxis = dict(title = 'Time'), yaxis = dict(title = 'Positive/ Negative compound'))
     figure = dict(data=Data([line, avg_ref]), layout=layout)
     py.offline.plot(figure, filename = 'Plots/sentiment-line.html')
 
+def plot_bar(data_list):
+    name = data_list[0]
+    x_axis = []
+    neg_bar, neu_bar, pos_bar = [], [], []
+    messages = []
+    for i in range(1, len(data_list)):
+        x_axis.append(datetime.datetime.fromtimestamp(data_list[i][2]).strftime('%Y-%m-%d %H:%M:%S'))  # Timestamp
+        neg_bar.append(data_list[i][1][1])  # Negative
+        neu_bar.append(data_list[i][1][2])  # Neutral
+        pos_bar.append(data_list[i][1][3])  # Positive
+        messages.append(data_list[i][0])    # Message
+
+    neg = {'x': x_axis, 'y': neg_bar, 'name': 'Negative', 'type': 'bar', 'marker': dict(color='rgb(237,28,36)')}
+    neu = {'x': x_axis, 'y': neu_bar, 'name': 'Neutral', 'type': 'bar', 'marker': dict(color='rgb(255,201,14)')}
+    pos = {'x': x_axis, 'y': pos_bar, 'text': messages, 'name': 'Positive', 'type': 'bar', 'marker': dict(color='rgb(34,177,76)')}
+
+    layout = dict(title = 'Data for <b>' + name +'</b>', xaxis = dict(title = 'Time'), yaxis = dict(title = 'Sentiment'), barmode = 'relative')
+    figure = dict(data=[neg, neu, pos], layout=layout)
+    py.offline.plot(figure, filename = 'Plots/sentiment-bar.html')
+
+
 zinloos = ["#kerst", ["het is bijna #kerst", [0.3, 0.2, 0.5, 0.3], 1375432865], ["heb zin in #kerst", [0.2, 0.1, 0.7, 0.2], 1375433865], ["ik hou zooo veel van #kerst", [0.8, 0.1, 0.2, 0.7], 1375436864], ["Wat heb ik een hekel aan #kerstmuziek", [-0.6, 0.6, 0.2, 0.2], 1375476592]]
-plot_line(zinloos)
+#plot_line(zinloos)
+plot_bar(zinloos)

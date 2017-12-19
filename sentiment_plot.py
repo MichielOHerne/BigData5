b6fba52e90951958263e8b30fe40cd8d86a776e4
Tim <19531735+TimAl@users.noreply.github.com>
2017-12-19 09:37:05 +0100

    Added moving average
    - added a moving average in the line-plot which takes the average of all messages within an hour (from hh:00:00 up to hh:59:59, point plotted in the middle at hh:30:00)

--- sentiment_plot.py
+++ sentiment_plot.py
@@ -7,18 +7,37 @@ def plot_line(data_list):
     name = data_list[0]
     x_axis = []
     y_axis = []
+    x_moving_avg = []
+    y_moving_data = []
     messages = []
     total = 0
     for i in range(1, len(data_list)):
-        x_axis.append(datetime.datetime.fromtimestamp(data_list[i][2]).strftime('%Y-%m-%d %H:%M:%S'))  # Timestamp
+        timestamp_e = datetime.datetime.fromtimestamp(data_list[i][2]).strftime('%Y-%m-%d %H:%M:%S') # Extended timestamp
+        timestamp_s = datetime.datetime.fromtimestamp(data_list[i][2]).strftime('%Y-%m-%d %H:30') # Short timestamp
+        x_axis.append(timestamp_e)
         y_axis.append(data_list[i][1][0])  # Compound
         messages.append(data_list[i][0])   # Message
         total = total + data_list[i][1][0] # Total compound
-    line = Scatter(x=x_axis, y=y_axis, text=messages, line=dict(shape='linear'), name='Positive/ Negative compound')
+        # Moving average data
+        if (timestamp_s in x_moving_avg):
+            y_moving_data[len(y_moving_data)-1].append(data_list[i][1][0])
+        else:
+            x_moving_avg.append(timestamp_s)
+            y_moving_data.append([data_list[i][1][0]])
     avg = total/(len(data_list)-1)
-    avg_ref = Scatter(x=[x_axis[0], x_axis[len(x_axis)-1]], y=[avg]*2, text=['Average']*2, line=dict(shape='spline', dash = 'dash'), name='Average')
-    layout = dict(title = 'Data for <b>' + name +'</b>', xaxis = dict(title = 'Time'), yaxis = dict(title = 'Positive/ Negative compound'))
-    figure = dict(data=Data([line, avg_ref]), layout=layout)
+    # Moving average
+    y_moving_avg = []
+    for moment in y_moving_data:
+        sum = 0
+        for item in moment:
+            sum = sum + item
+        y_moving_avg.append(sum/len(moment))
+    # Lines
+    line = Scatter(x=x_axis, y=y_axis, text=messages, line=dict(shape='linear'), name='Positive/ Negative compound')
+    avg_ref = Scatter(x=[x_axis[0], x_axis[len(x_axis)-1]], y=[avg]*2, text=['Average']*2, line=dict(shape='spline', dash = 'dash'), name='Total average')
+    mov_avg = Scatter(x=x_moving_avg, y=y_moving_avg, text=x_moving_avg, line=dict(shape='spline'), name='Moving average (hour)')
+    layout = dict(title = 'Data for <b>' + name +'</b>', xaxis = dict(title = 'Time'), yaxis = dict(title = 'Positive/ Negative compound', range = [-1, 1]))
+    figure = dict(data=Data([line, mov_avg, avg_ref]), layout=layout)
     py.offline.plot(figure, filename = 'Plots/sentiment-line.html')
 
 def plot_bar(data_list):
@@ -42,6 +61,6 @@ def plot_bar(data_list):
     py.offline.plot(figure, filename = 'Plots/sentiment-bar.html')
 
 
-zinloos = ["#kerst", ["het is bijna #kerst", [0.3, 0.2, 0.5, 0.3], 1375432865], ["heb zin in #kerst", [0.2, 0.1, 0.7, 0.2], 1375433865], ["ik hou zooo veel van #kerst", [0.8, 0.1, 0.2, 0.7], 1375436864], ["Wat heb ik een hekel aan #kerstmuziek", [-0.6, 0.6, 0.2, 0.2], 1375476592]]
-#plot_line(zinloos)
-plot_bar(zinloos)
+zinloos = ["#kerst", ["het is bijna #kerst", [0.3, 0.2, 0.5, 0.3], 1375432865], ["heb zin in #kerst", [0.2, 0.1, 0.7, 0.2], 1375433865], ["ik hou zooo veel van #kerst", [0.8, 0.1, 0.2, 0.7], 1375436864], ["Wat praten we over #kerst in de zomer?", [0.2, 0.1, 0.7, 0.2], 1375458723], ["Het is #kerst over 145 dagen!", [0.3, 0, 0.6, 0.4], 1375474832], ["Wat heb ik een hekel aan #kerstmuziek", [-0.6, 0.6, 0.2, 0.2], 1375476592], ["Zo blij! Ik hoef helemaal niet aan #kerstinkopen te denken", [0.5, 0.1, 0.2, 0.7], 1375476912]]
+plot_line(zinloos)
+#plot_bar(zinloos)

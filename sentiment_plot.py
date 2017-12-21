44b11a0ffe8a477ad76531b2a374f80554e7cd8f
Tim <19531735+TimAl@users.noreply.github.com>
2017-12-21 13:56:27 +0100

    Added functionality to World-map
    Possible to see in which country the massage occurs the most. To switch between different modes;
    map_mode = 'sentiment'    ->   shows the average sentiment per country (default)
    map_mode = 'occurrence'   ->   shows which country delivers the most topics mentioning the hashtag
    See also the help-function of the corresponding function.

--- sentiment_plot.py
+++ sentiment_plot.py
@@ -80,7 +80,7 @@ def plot_bar(data_list, color_neg='rgb(149,28,28)', color_neu='rgb(166,118,42)',
     py.offline.plot(figure, filename='Plots/sentiment-bar.html')
 
 
-def plot_world(data_list, color_neg='rgb(149,28,28)', color_neu='rgb(166,118,42)', color_pos='rgb(128,192,50)', loc_mode="ISO-3"):
+def plot_world(data_list, color_neg='rgb(149,28,28)', color_neu='rgb(166,118,42)', color_pos='rgb(128,192,50)', loc_mode="ISO-3", map_mode='sentiment'):
     ''' Returns an interactive world map with the data in the directory "Plots/"
         Takes as argument a list with:
           list[0] = name (str)
@@ -92,12 +92,16 @@ def plot_world(data_list, color_neg='rgb(149,28,28)', color_neu='rgb(166,118,42)
           list[1:][3] = country_code (str) -> No error message when incorrect!
         Optional arguments are the colors in the compound-scale
         Optional argument, switch between country names or the three letter name
-          loc_mode="country names" or "ISO-3" '''
+          loc_mode="country names" or "ISO-3"
+        Optional argument, switch between what which is displayed in the map: map_mode=
+          'sentiment': Plot of the sentiment per country (default)
+          'occurrence': Plot how much a topic is mentioned '''
     timestamp = [datetime.datetime.fromtimestamp(data_list[1][2]).strftime('%d-%m-%Y (%H:%M)'), datetime.datetime.fromtimestamp(data_list[len(data_list)-1][2]).strftime('%d-%m-%Y (%H:%M)')]
     name = 'Data for <b>'+data_list[0]+'</b><br><span style="font-size:64%;"><i>'+str(len(data_list)-1)+' messages between '+timestamp[0]+' and '+timestamp[1]+'</i></span>'
     country = []
     compound = []
     nom = []  # number of messages
+    info = []
     # Merge messages from the same country
     for i in range(1, len(data_list)):
         duplicate = False
@@ -115,9 +119,16 @@ def plot_world(data_list, color_neg='rgb(149,28,28)', color_neu='rgb(166,118,42)
         for j in range(len(compound[i])):
             tmp = tmp + compound[i][j]
         tmp = round(tmp/len(compound[i]), 3)
-        nom.append("Messages: "+str(len(compound[i]))+"<br>Compound: "+str(tmp))
+        info.append("Messages: "+str(len(compound[i]))+"<br>Compound: "+str(tmp))
+        nom.append(len(compound[i]))
         compound[i] = tmp
-    data = [dict(type='choropleth', locations=country, z=compound, text=nom, zmin=-1, zmax=1, marker=dict(line=dict(color='rgb(180,180,180)', width=0.5)), colorscale=[[0, color_neg], [0.5, color_neu], [1, color_pos]], colorbar=dict(title="Compound"), hoverinfo='text+location', locationmode=loc_mode)]
+    if map_mode == 'sentiment':
+        data = [dict(type='choropleth', locations=country, z=compound, text=info, zmin=-1, zmax=1, marker=dict(line=dict(color='rgb(180,180,180)', width=0.5)), colorscale=[[0, color_neg], [0.5, color_neu], [1, color_pos]], colorbar=dict(title="Compound"), hoverinfo='text+location', locationmode=loc_mode)]
+    elif map_mode == 'occurrence':
+        data = [dict(type='choropleth', locations=country, z=nom, text=info, zmin=0, marker=dict(line=dict(color='rgb(180,180,180)', width=0.5)), colorscale=[[0, color_neg], [0.5, color_neu], [1, color_pos]], colorbar=dict(title="occurrence"), hoverinfo='text+location', locationmode=loc_mode)]
+    else:
+        print("Unknown map_mode")
+        data = None
     layout = dict(title=name, geo=dict(showframe=False, showland=True, landcolor="rgb(192, 192, 192)", showcoastlines=True, projection=dict(type='Mercator')))
     fig = dict(data=data, layout=layout)
     py.offline.plot(fig, validate=False, filename='Plots/sentiment-world.html')
@@ -166,4 +177,4 @@ plot_bar(zinloos)
 plot_world(zinloos)
 plot_pie(zinloos)
 # Example to get free help
-help(plot_pie)
+help(plot_world)

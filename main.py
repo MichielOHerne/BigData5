16eab9e7cf8a532209f6698dc95d65af16f3ba13
CasperSpronk <33654486+CasperSpronk@users.noreply.github.com>
2018-01-09 10:15:30 +0100

    Add files via upload
    this is the main loop that will run all code

---
+++ main.py
@@ -0,0 +1,28 @@
+from sort_for_graphing import open_json, open_json_for_hashtag
+from importingtwitterdata_new import import_data
+from plot_bar import plot_bar
+from plot_line import plot_line
+from plot_pie import plot_pie
+from plot_world import plot_world
+print("done importing")
+all_hashtags = []
+
+
+def sort_hash(data, col):
+    new_data = []
+    new_data.extend(sorted(data[0:], key=lambda data: -data[col]))
+    return new_data
+
+
+data = import_data()
+all_hashtags = open_json_for_hashtag("twitterdata.json", all_hashtags)
+print(all_hashtags)
+sorted_all_hashtags = sort_hash(all_hashtags, 1)
+print(sorted_all_hashtags)
+for i in range(len(sorted_all_hashtags)):
+    if all_hashtags[i][1] >= 20:
+        sent_tweets = open_json("twitterdata.json", [sorted_all_hashtags[i][0]])
+        plot_line(sent_tweets)
+        plot_bar(sent_tweets)
+        plot_pie(sent_tweets)
+        plot_world(sent_tweets)

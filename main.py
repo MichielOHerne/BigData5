f949257e7aee72da916c6931d73b0488582f6087
CasperSpronk <33654486+CasperSpronk@users.noreply.github.com>
2018-01-09 10:32:34 +0100

    Update main.py
    now only prints charts for most popular hashtag

--- main.py
+++ main.py
@@ -19,10 +19,18 @@ all_hashtags = open_json_for_hashtag("twitterdata.json", all_hashtags)
 print(all_hashtags)
 sorted_all_hashtags = sort_hash(all_hashtags, 1)
 print(sorted_all_hashtags)
-for i in range(len(sorted_all_hashtags)):
-    if all_hashtags[i][1] >= 20:
-        sent_tweets = open_json("twitterdata.json", [sorted_all_hashtags[i][0]])
-        plot_line(sent_tweets)
-        plot_bar(sent_tweets)
-        plot_pie(sent_tweets)
-        plot_world(sent_tweets)
+
+sent_tweets = open_json("twitterdata.json", [sorted_all_hashtags[0][0]])
+plot_line(sent_tweets)
+plot_bar(sent_tweets)
+plot_pie(sent_tweets)
+plot_world(sent_tweets)
+
+
+# for i in range(len(sorted_all_hashtags)):
+#     if all_hashtags[i][1] >= 20:
+#         sent_tweets = open_json("twitterdata.json", [sorted_all_hashtags[i][0]])
+#         plot_line(sent_tweets)
+#         plot_bar(sent_tweets)
+#         plot_pie(sent_tweets)
+#         plot_world(sent_tweets)
